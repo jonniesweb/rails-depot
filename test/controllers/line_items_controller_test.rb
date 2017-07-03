6862dc0220a1259e107547e0cd8558bb0a3e6db1
Jon Simpson <jon@jonsimpson.ca>
2017-07-03 00:18:31 -0400

    calculate total price and line item price

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,7 +23,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
 
     assert_select 'h2', 'Your cart'
-    assert_select '#main li', "1 \u00D7 Programming Ruby 1.9"
+    assert_select '#main td', "Programming Ruby 1.9"
   end
 
   test "should show line_item" do
