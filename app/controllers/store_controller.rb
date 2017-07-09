680404941ea0634acc4da755d8ef164ffaea8abf
Jon Simpson <jon@jonsimpson.ca>
2017-07-09 19:57:12 -0400

    fix small test regressions

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -6,7 +6,7 @@ class StoreController < ApplicationController
 
   def index
     if params[:set_locale]
-      redirect_to store_index_url, locale: params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
     else
       @products = Product.order(:title)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   end
 
   resources :users
+  
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   scope '(:locale)' do
     resources :orders
@@ -16,8 +20,5 @@ Rails.application.routes.draw do
     root 'store#index', as: 'store_index', via: :all
   end
 
-  resources :products do
-    get :who_bought, on: :member
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
